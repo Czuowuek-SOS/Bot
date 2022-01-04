697970a44d69f2f4b3d3de5aba71c4e4d5bb4308
amogus <91487506+Czuowuek-SOS@users.noreply.github.com>
2022-01-04 18:20:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,32 @@ import sys
 import os
 import pickle
 import ctypes
-from data import join_channel
+import platform
 
-import discord
+from nextcord import guild
+from data import join_channel
+from data import Token
+
+systemInfo = platform.uname()
+
+try:
+    import nextcord as discord
+except ImportError:
+    if systemInfo.system == 'Windows':
+        os.startfile(f'{os.getcwd()}/pkg-install.ps1')
+        import nextcord as discord
+    elif systemInfo.system == 'Linux' or systemInfo.system == 'MacOS':
+        os.system('python3 -m pip install -U discord.py')
+        import nextcord as discord
+
 from discord import user
 from discord import member
 from discord import Message
-from discord.message import Author
 from discord import channel
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 
-token = 'sex'
+token = Token.token
 client = commands.Bot(command_prefix='.')
 
 kernel32 = ctypes.WinDLL('kernel32')
@@ -52,18 +66,22 @@ async def on_ready():
     print(Colors.green+'Online')
     kernel32.SetConsoleTitleW('Trader Bot')
 
+# @client.event
+# async def on_guild_join():
+#     async guild.join_channel.send('')
+
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.MissingPermissions):
         await ctx.channel.send('ogólnie nie możesz wykonać tej operacji bo coś z permisjami jest nie tak ale nie wiem co, mam nadzieję żę pomogłem')
     else:
-        await ctx.channel.send('coś jebło ale nie wiem co')
+        await ctx.channel.send(f'''```py\n{error}\n```''')
 
 # sets commands
 
 @client.command()
-async def set_join_chanel (ctx, channel : discord.channel):
-    ctx.channel.send(f'Ustawiono kanał przywirań na {channel} \n wymagane ponowne uruchomienie bota')
+async def set_join_channel (ctx, channel : discord.channel):
+    ctx.channel.send(f'Ustawiono kanał przywitań na {channel} \n wymagane ponowne uruchomienie bota')
 
     with open(os.getcwd + '/.DIscord-Trader/data/join-channel.py', 'w') as w:
         w.write(f'joinChannel = {channel}')
@@ -75,11 +93,11 @@ async def set_join_chanel (ctx, channel : discord.channel):
 
 @client.command()
 @has_permissions(kick_members=True)
-async def wyjeb(ctx, member : discord.Member, *, reason=None):
+async def kick(ctx, member : discord.Member, *, reason=None):
     await ctx.guild.kick(member)
-    await ctx.channel.send(f"wyjebano {member.mention} za {reason}")
+    await ctx.channel.send(f"@{ctx.author.id} wyjebał {member.mention} za {reason}")
 
-    Log(f'{Message.author.id} wyrzucił {member} za {reason} \n')
+    Log(f'@{ctx.author.id} wyrzucił {member} za {reason} \n')
 
 
 
@@ -87,20 +105,27 @@ async def wyjeb(ctx, member : discord.Member, *, reason=None):
 @has_permissions(ban_members=True)
 async def ban(ctx, member : discord.Member, *, reason=None):
     await ctx.guild.ban(member)
-    await ctx.channel.send(f'{Message.author} zbanował {member} za {reason}')
+    await ctx.channel.send(f'@{ctx.author.id} zbanował {member} za {reason}')
 
 
+@client.command()
+async def end(ctx):
+    await ctx.channel.send('wyłonczanie bota...')
+    print(Colors.red+'Offline')
+    sys.exit(0)
 
 # user commands
 
 @client.command()
 async def random_choice(ctx, choice1 : str, choice2 : str):
-    choice = random.choice(choice1, choice2)
+    choice = random.choice([choice1, choice2])
     await ctx.channel.send(choice)
 
 @client.command()
-async def ranndom_Int(ctx, min, max):
-    await ctx.channel.send(str(random.randint(int(min), int(max))))
+async def ranndom_Int(ctx, min : int, max : int):
+    if max > 2137:
+        await ctx.channel.send('maksymalna wartość INT dla zmiannej max to 2137')
+    await ctx.channel.send(str(random.randint(min, max)))
 
 @client.command()
 async def twoja(ctx):
@@ -109,20 +134,45 @@ async def twoja(ctx):
 @client.command()
 async def zelson(ctx):
     await ctx.channel.send('E')
-    await time.sleep(1)
+    time.sleep(1)
     await ctx.channel.send('XD')
-    await time.sleep(1)
+    time.sleep(1)
     await ctx.channel.send('Kto pytal')
 
 @client.command()
 async def send(ctx, mes):
-    await ctx.channel.delete()
+    await ctx.channel.purge(limit=1)
     await ctx.channel.send(f'{mes}')
 
 @client.command()
-async def end(ctx):
-    await ctx.channel.send('wyłonczanie bota...')
-    print(Colors.red+'Offline')
-    sys.exit()
+async def sex(ctx, member : discord.member):
+    await ctx.channel.send(f'@{ctx.author.id} zaczoł ostro zapinać w dupe {member}')
+    time.sleep(5)
+    await ctx.channel.send(f'@{ctx.author.id} spuscił sie {member} prosto do dupy')
+
+@client.command()
+async def mati(ctx):
+    ctx.channel.send('''
+Samo bawienie się w politykow
+Już jest posrane
+I trzeba mieć źle w głowie
+Żeby kurwą w jakieś politykowanie się bawic
+Biorąc pod uwage
+Że średnia wieku tej spolecznosci to 14 lat 
+''')
+
+@client.command()
+async def aszer(ctx):
+    for i in range(69):
+        await ctx.channel.send(f'<@{ctx.author.id}> rel')
+
+@client.command()
+async def atack(ctx, integer : int, mamber : discord.member):
+    if integer > 69:
+        await ctx.channel.send('nie')
+        
+    await ctx.channel.purge(limit=1)
+    for i in range(integer):
+        ctx.channel.send(f'<@{member.id}>')
 
 client.run(token)
